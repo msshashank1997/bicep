902eb7e29f863d0973f0a530fca3a23283c64e5d
msshashank1997 <139876587+msshashank1997@users.noreply.github.com>
2023-07-27 12:22:41 +0530

    Update deploybicep.ps1

--- windows-vms/deploybicep.ps1
+++ windows-vms/deploybicep.ps1
@@ -1,6 +1,27 @@
-Connect-AzAccount
 $DeploymentID = 'DID'
 $rgname = 'host-'+$DeploymentID
+
+#region Login
+# This logs into Azure with a Service Principal Account
+#
+Write-Output "Logging in to Azure with a service principal..."
+az login `
+    --service-principal `
+    --username $servicePrincipal `
+    --password $servicePrincipalSecret `
+    --tenant $servicePrincipalTenantId
+Write-Output "Done"
+Write-Output ""
+#endregion
+
+#region Subscription
+#This sets the subscription the resources will be created in
+Write-Output "Setting default azure subscription..."
+az account set `
+    --subscription $azureSubscriptionName
+Write-Output "Done"
+Write-Output ""
+#endregion
 
 New-AzResourceGroup -Name $rgname -Location "eastus"
 
