27dfc0de82db53a44a0cab90942f74db49ad87c0
msshashank1997 <139876587+msshashank1997@users.noreply.github.com>
2023-07-28 16:53:51 +0530

    Update create-vm-azure.ps1

--- powershell_scripts/create-vm-azure.ps1
+++ powershell_scripts/create-vm-azure.ps1
@@ -1,30 +1,49 @@
-# Login to Azure using Service Principal credentials from Github Secrets
+#region Login
+# This logs into Azure with a Service Principal Account
+#
 Write-Output "Logging in to Azure with a service principal..."
 az login `
     --service-principal `
-    --username $Env:SERVICE_PRINCIPAL_APPID `
-    --password $Env:SERVICE_PRINCIPAL_SECRET `
-    --tenant $Env:SERVICE_PRINCIPAL_TENANTID
+    --username $servicePrincipal `
+    --password $servicePrincipalSecret `
+    --tenant $servicePrincipalTenantId
 Write-Output "Done"
+Write-Output ""
+#endregion
 
-# Select Azure subscription
+#region Subscription
+#This sets the subscription the resources will be created in
+Write-Output "Setting default azure subscription..."
 az account set `
-    --subscription $Env:AZURE_SUBSCRIPTION_NAME
+    --subscription $azureSubscriptionName
+Write-Output "Done"
+Write-Output ""
+#endregion
 
-# Create the VM configuration object
-$ResourceGroupName = "bicep"
-$VmName = "test-123"
+#region Create Resource Group
+# This creates the resource group used to house the VM
+Write-Output "Creating resource group $resourceGroupName in region $resourceGroupNameRegion..."
+az group create `
+    --name $resourceGroupName `
+    --location $resourceGroupNameRegion
+    Write-Output "Done creating resource group"
+    Write-Output ""
+#endregion
 
-# Create a VM in Azure
+#region Create VM
+# Create a VM in the resource group
 Write-Output "Creating VM..."
 try {
     az vm create  `
-        --resource-group $ResourceGroupName `
-        --name $VmName `
+        --resource-group $resourceGroupName `
+        --name $serverName `
         --image win2016datacenter `
-        --admin-password $Env:SP_CLIENT_SECRET `
+        --admin-username $adminLogin `
+        --admin-password $adminPassword
     }
 catch {
     Write-Output "VM already exists"
     }
 Write-Output "Done creating VM"
+Write-Output ""
+#endregion
